85c40733d5c7996fce53a0a3e01f73448a63a545
paulgurpreet <gurpreetpaul02@gmail.com>
2024-11-07 23:47:57 +0530

    Update PYTHON.py

--- PYTHON.py
+++ PYTHON.py
@@ -1,4 +1,5 @@
 #PRACTICAL-1
+#WAP to find the roots of a quadratic equation
 
 a,b,c= eval(input("enter values( of a,b,constant) separated by commas : "))
 
@@ -11,7 +12,12 @@ else:
     print("roots are not real")
 
 #PRACTICAL-2
-(a)
+#wAP to accept a number ‘n’ and  
+#(a)Check if ’n’ is prime 
+#(b)Generate all prime numbers till ‘n’  
+#(c)Generate first ‘n’ prime numbers This program may be done using functions 
+
+#(a)
 n = eval(input("enter value :"))
 if n>1:
   for i in range(2,n):
@@ -24,7 +30,7 @@ if n>1:
 else:
         print(n,"is not a prime number ")
 
-(b)
+#(b)
 n = eval(input("enter value "))
 for num in range(1,n):
     if num > 1:
@@ -33,7 +39,7 @@ for num in range(1,n):
             break
         else:
             print(num,end =',')
-(c)
+#(c)
 n = eval(input("enter value "))
 count = 0
 number = 2
@@ -48,6 +54,17 @@ while count < n:
         number += 1
 
 #PRACTICAL-3
+#WAP to create a pyramid of the character ‘*’ and a reverse pyramid 
+#* 
+#*** 
+#***** 
+#******* 
+#********* 
+#            ********* 
+#            ******* 
+#           ****** 
+#           *** 
+#           * 
 
 n = int(input("enter the no of rows :"))
 for i in range(n):
@@ -64,6 +81,11 @@ for i in range (n):
 print()
 
 #PRACTICAL-4
+#WAP that accepts a character and performs the following: 
+#(a). print whether the character is a letter or numeric digit or a special character 
+#(b). if the character is a letter, print whether the letter is uppercase or lowercase 
+#(c). if the character is a numeric digit, prints its name in text (e.g., if input is 9, 
+#output is NINE) 
 
 character = input("enter data ")
 if character >='A' and character<='Z':
@@ -79,8 +101,13 @@ else:
  print("special character")
 
 #PRACTICAL-5
+#WAP to perform the following operations on a string 
+#(a). Find the frequency of a character in a string. 
+#(b). Replace a character by another character in a string. 
+#(c). Remove the first occurrence of a character from a string. 
+#(d). Remove all occurrences of a character from a string. 
 
-(a)
+#(a)
 string = "hello welcome to python"
 character = input("enter a character :")
 f = 0
@@ -89,20 +116,21 @@ for i in string:
   f += 1
 print("frequency of",character,'is',f)
 
-(b)
+#(b)
 string = "hello welcome to python"
 print(string.replace("h","t"))
 
-(c)
+#(c)
 string = "hello welcome to python"
 print(string[1:len(string)])
 
-(d)
+#(d)
 
 string = "hello welcome to python"
 print(string[:0])
 
 #PRACTICAL-6
+# WAP to swap the first n characters of two strings
 str1 = input("enter first string: ")
 str2 = input("enter second string: ")
 n1 = int(input("enter no of character which is to be swap: "))
@@ -114,6 +142,9 @@ else:
  print(str2.replace(m,n))
 
 #PRACTICAL-7
+# Write a function that accepts two strings and returns the indices of all the occurrences 
+#of the second string in the first string as a list. If the second string is not present in the 
+#first string then it should return -1. 
 
 def occurances(a,b):
  newlist =[]
@@ -133,5 +164,85 @@ b= input("enter second string ;")
 occurances(a,b)
 
 # PRACTICAL-8
+# WAP to create a list of the cubes of only the even integers appearing in the input list 
+#(may have elements of other types also) using the following: 
+#(a). 'for' loop 
+#(b).  list comprehension
+
+def cubes():
+ newlist= []
+ number = [1,2,3,4,5,6,"seven"]
+ for i in number:
+     if type(i)== int:
+         if i % 2 == 0:
+             newlist.append(i**3)
+         print(newlist)
+cubes()
+# PRACTICAL-9
+
+#PRACTICAL-10
+
+
+
+# PRACTICAL-11
+#write a function that prints a dictionary where the keys are numbers
+#between 1 and 5 and the values are cubes of the keys.
+
+def cubes():
+ dict={}
+ for i in range (1,6):
+    dict[i]=i**3
+ print(dict)
+cubes()
+
+# PRACTICAL-12
+#) Consider a tuple t1=(1,2,5,7,9,2,4,6,8,10).WAP to perform following
+#oprations:
+#(a.)Print half the values of the tuple in one line and other half in the other next
+#line.
+#(b.)Print another tuple whose values are even numbers in the given tuple.
+#(c.)concatenate a tuple t2 = (11,13,15) with t1.
+#(d.)Return maximmum and minimum value from this given tuple.
+
+#(a)
+t1 = (1,2,5,7,9,2,4,6,8,10)
+half_value=len(t1)//2
+first_half = t1[ :half_value]
+print("first_half",first_half)
+second_half = t1[half_value: ]
+print("second_half",second_half)
+
+#(b)
+t1 = (1,2,5,7,9,2,4,6,8,10)
+even_number= tuple(filter(lambda x: x%2==0,t1))
+print("tuple with even number",even_number)
+
+#(c)
+t1 = (1,2,5,7,9,2,4,6,8,10)
+t2 = (11,13,15)
+concatenation= (t1 + t2)
+print("tuple with concatenation ", concatenation)
+
+#(d)
+t1= (1, 2, 5, 7, 9, 2, 4, 6, 8, 10, 11, 13, 15)
+print("maximum value in t1 is ",max(t1))
+print("minimum value in t1 is ",min(t1))
+
+# PRACTICAL -13
+#Wap to accept a name from a user. Raise and handle appropriate
+#exception(s) if the text entered by the user contains digits and/or special
+#characters.
+
+name = input("enter a name ")
+try:
+ if name.isalpha():
+     print("This is correct Name")
+ else:
+    raise Exception(" There is NameError")
+except Exception as e:
+ print(e)
+
+
+
 
           
